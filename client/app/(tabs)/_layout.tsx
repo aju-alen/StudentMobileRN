3e69bd8ad8d5ee04cfd3f2697f3ab34c00c265c5
Viveks399 <vivek.me.619@gmail.com>
2024-01-02 13:30:24 +0530

    Community Page

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,46 +1,50 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import React from 'react'
-import { Tabs } from 'expo-router'
-
-
-
+import Ionicons from "@expo/vector-icons/Ionicons";
+import React from "react";
+import { Tabs } from "expo-router";
 
 const TabsLayout = () => {
-    
   return (
-    
-    <Tabs  screenOptions={({route}) => ({
-      tabBarIcon: ({focused, size}) => {
-        let iconName;
-        if (route.name === 'home/index') {
-          iconName = focused ? 'home' : 'home-outline';
-        } else if (route.name === 'community/index') {
-          iconName = focused ? 'people' : 'people-outline';
-        }
-         else if (route.name === 'chat/index') {
-          iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
-        }
-         else if (route.name === 'profile/index') {
-          iconName = focused ? 'person' : 'person-outline';
-        }
-        return <Ionicons name={iconName} size={size} color={'black'} />;
-      },
-      tabBarLabelStyle: {
-        color: 'black',
-      },
- tabBarStyle: {
-        backgroundColor: 'white',
-      },
-      
-     
-    })}>  
-        
-        <Tabs.Screen name='home/index' options={{ headerShown: false, tabBarLabel:"Home" }} />
-        <Tabs.Screen name='community/index' options={{ headerShown: false, tabBarLabel:"Community" }}/>
-        <Tabs.Screen name='chat/index' options={{ headerShown: false, tabBarLabel:"Chat" }}/>
-        <Tabs.Screen name='profile/index' options={{ headerShown: false, tabBarLabel:"Profile" }}/>
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, size }) => {
+          let iconName;
+          if (route.name === "home/index") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "community/index") {
+            iconName = focused ? "people" : "people-outline";
+          } else if (route.name === "chat/index") {
+            iconName = focused ? "chatbubbles" : "chatbubbles-outline";
+          } else if (route.name === "profile/index") {
+            iconName = focused ? "person" : "person-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={"black"} />;
+        },
+        tabBarLabelStyle: {
+          color: "black",
+        },
+        tabBarStyle: {
+          backgroundColor: "white",
+        },
+      })}
+    >
+      <Tabs.Screen
+        name="home/index"
+        options={{ headerShown: false, tabBarLabel: "Home" }}
+      />
+      <Tabs.Screen
+        name="community/index"
+        options={{ headerShown: false, tabBarLabel: "Community" }}
+      />
+      <Tabs.Screen
+        name="chat/index"
+        options={{ headerShown: false, tabBarLabel: "Chat" }}
+      />
+      <Tabs.Screen
+        name="profile/index"
+        options={{ headerShown: false, tabBarLabel: "Profile" }}
+      />
     </Tabs>
-  )
-}
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
