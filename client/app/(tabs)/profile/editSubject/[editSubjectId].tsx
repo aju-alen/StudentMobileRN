1b82fa5db7eddf729abc614bdd7efc33ab78768f
Viveks399 <vivek.me.619@gmail.com>
2024-01-11 12:51:56 +0530

    created edit option for single subjects

--- client/app/(tabs)/profile/editSubject/[editSubjectId].tsx
+++ client/app/(tabs)/profile/editSubject/[editSubjectId].tsx
@@ -1,14 +1,214 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-const EditSingleSubject = () => {
-  return (
-    <View>
-      <Text>EditSingleSubject</Text>
-    </View>
-  )
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { ipURL } from "../../../utils";
+import { useLocalSearchParams } from "expo-router";
+interface SubjectData {
+  subjectImage?: string;
+  subjectName?: string;
+  subjectBoard?: string;
+  subjectDescription?: string;
+  subjectGrade?: number;
+  subjectPrice?: number;
+  subjectTags?: [string];
 }
 
-export default EditSingleSubject
+const EditSingleSubject = () => {
+  const [subjectName, setSubjectName] = useState("");
+  const [subjectDescription, setSubjectDescription] = useState("");
+  const [subjectImage, setSubjectImage] = useState("");
+  const [subjectPrice, setSubjectPrice] = useState("");
+  const [subjectBoard, setSubjectBoard] = useState("");
+  const [subjectGrade, setSubjectGrade] = useState("");
+  const [subjectLanguage, setSubjectLanguage] = useState("");
+  const [skillTags, setSkillTags] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [singleSubjectData, setSingleSubjectData] = React.useState<SubjectData>(
+    {}
+  );
+  const { editSubjectId } = useLocalSearchParams();
+  console.log("local search params", useLocalSearchParams());
 
-const styles = StyleSheet.create({})
+  useEffect(() => {
+    const getSubjects = async () => {
+      try {
+        const token = await AsyncStorage.getItem("authToken");
+        const resp = await axios.get(
+          `http://${ipURL}/api/subjects/${editSubjectId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log("this is resp", resp.data);
+
+        setSingleSubjectData(resp.data);
+      } catch (error) {
+        console.error("Error fetching subject data:", error);
+      }
+    };
+
+    getSubjects();
+  }, []);
+
+  const handleCreateSubject = async () => {
+    const subject = {
+      subjectName,
+      subjectDescription,
+      subjectImage,
+      subjectPrice,
+      subjectBoard,
+      subjectGrade,
+      subjectLanguage,
+      skillTags,
+    };
+    console.log("this is subject", subject);
+  };
+
+  const handleInputChange = (text) => {
+    setInputText(text);
+  };
+  const handleAddItem = () => {
+    if (inputText.trim() !== "") {
+      setSkillTags([...skillTags, inputText]);
+      setInputText("");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Ionicons name="book-outline" size={20} color="#900" />
+        <TextInput
+          style={styles.input1}
+          placeholder="Subject Name"
+          value={subjectName}
+          onChangeText={setSubjectName}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Ionicons name="book-outline" size={20} color="#900" />
+        <TextInput
+          style={styles.input1}
+          placeholder="Subject Description"
+          value={subjectDescription}
+          onChangeText={setSubjectDescription}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Ionicons name="book-outline" size={20} color="#900" />
+        <TextInput
+          style={styles.input1}
+          placeholder="Subject Image"
+          value={subjectImage}
+          onChangeText={setSubjectImage}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Ionicons name="book-outline" size={20} color="#900" />
+        <TextInput
+          style={styles.input1}
+          placeholder="Subject Price"
+          value={subjectPrice}
+          onChangeText={setSubjectPrice}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Ionicons name="book-outline" size={20} color="#900" />
+        <TextInput
+          style={styles.input1}
+          placeholder="Subject Board"
+          value={subjectBoard}
+          onChangeText={setSubjectBoard}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Ionicons name="book-outline" size={20} color="#900" />
+        <TextInput
+          style={styles.input1}
+          placeholder="Subject Grade"
+          value={subjectGrade}
+          onChangeText={setSubjectGrade}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Ionicons name="book-outline" size={20} color="#900" />
+        <TextInput
+          style={styles.input1}
+          placeholder="Subject Language"
+          value={subjectLanguage}
+          onChangeText={setSubjectLanguage}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Ionicons name="construct-outline" size={20} color="#900" />
+        <TextInput
+          style={styles.input1}
+          value={inputText}
+          onChangeText={handleInputChange}
+          placeholder="Add One Skill At A Time"
+        />
+        <TouchableOpacity onPress={handleAddItem}>
+          <View style={styles.addButton}>
+            <Ionicons name="add-circle-outline" size={20} color="#900" />
+            <Text>Add</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleCreateSubject}>
+        <Text style={styles.text}>Create Your Subject</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default EditSingleSubject;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 12,
+    paddingHorizontal: 8,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: "gray",
+  },
+  input1: {
+    flex: 1,
+    height: 40,
+    paddingHorizontal: 8,
+  },
+  addButton: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "#b54034",
+    borderRadius: 100,
+    paddingHorizontal: 8,
+    paddingVertical: 2,
+  },
+  text: {
+    fontSize: 18,
+    fontFamily: "Roboto-Regular",
+    color: "white",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#b54034",
+    padding: 10,
+    borderRadius: 100,
+    margin: 12,
+  },
+});
--- client/app/components/HomeFlatlist.tsx
+++ client/app/components/HomeFlatlist.tsx
@@ -6,13 +6,53 @@ import {
   FlatList,
   Image,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { router, usePathname } from "expo-router";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ipURL } from "../utils";
 
 const HomeFlatlist = ({ homeData, handleItemPress }) => {
-  console.log(homeData, "child component");
-
   const [showAllText, setShowAllText] = useState(false);
   const maxLines = 2;
+  console.log("homeData", homeData);
+
+  const handlePressEdit = (item) => {
+    console.log("Edit item", item._id);
+    router.push(`../profile/editSubject/${item._id}`);
+  };
+  const handleIPressDelete = (item) => {
+    console.log("Delete item", item);
+  };
+  const routeInfo = usePathname();
+  interface User {
+    isTeacher?: boolean;
+    subjects?: SubjectItem[];
+  }
+  interface SubjectItem {
+    _id: string;
+    subjectName: string;
+    subjectDescription?: string;
+    subjectImage?: string;
+    subjectPrice?: number;
+    subjectBoard?: string;
+    subjectGrade?: number;
+  }
+
+  const [user, setUser] = useState<User>({});
+  useEffect(() => {
+    const getUser = async () => {
+      const apiUser = await axios.get(`http://${ipURL}/api/auth/me`, {
+        headers: {
+          Authorization: `Bearer ${await AsyncStorage.getItem("authToken")}`,
+        },
+      });
+      setUser(apiUser.data);
+    };
+    getUser();
+  }, []);
+
+  console.log("User>>>>", user);
 
   return (
     <View style={{ flex: 1 }}>
@@ -23,9 +63,25 @@ const HomeFlatlist = ({ homeData, handleItemPress }) => {
             <View style={styles.card}>
               <View style={styles.titlePriceContainer}>
                 <Text style={styles.text1}>{item.subjectName}</Text>
-                <Text style={[styles.text2, styles.button2]}>
-                  {item.subjectBoard}
-                </Text>
+                <View>
+                  <Text style={[styles.text2, styles.button2]}>
+                    {item.subjectBoard}
+                  </Text>
+                  {routeInfo === "/profile" && user.isTeacher && (
+                    <View style={styles.editButtonContainer}>
+                      <TouchableOpacity onPress={() => handlePressEdit(item)}>
+                        <Text style={[styles.text2, styles.button2]}>Edit</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                        onPress={() => handleIPressDelete(item)}
+                      >
+                        <Text style={[styles.text2, styles.button2]}>
+                          Delete
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                  )}
+                </View>
               </View>
               <View style={styles.rowContainer}>
                 <Image
@@ -54,15 +110,6 @@ const HomeFlatlist = ({ homeData, handleItemPress }) => {
 export default HomeFlatlist;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 40,
-    fontFamily: "Roboto-Regular",
-  },
   text1: {
     fontSize: 20,
     fontFamily: "Roboto-Regular",
@@ -71,27 +118,6 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontFamily: "Roboto-Regular",
   },
-  button: {
-    marginTop: 10,
-    padding: 10,
-    backgroundColor: "blue",
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  searchInput: {
-    width: "100%",
-    height: "100%",
-  },
-  searchContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-    height: 50,
-  },
   card: {
     backgroundColor: "white",
     borderRadius: 8,
@@ -99,7 +125,7 @@ const styles = StyleSheet.create({
     borderColor: "#ddd",
     padding: 10,
     margin: 10,
-    shadowColor: "#000",
+    shadowColor: "orange",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
@@ -125,19 +151,16 @@ const styles = StyleSheet.create({
     paddingLeft: 2,
     paddingBottom: 3,
   },
-  button1: {
-    backgroundColor: "#808080",
-    paddingLeft: 5,
-    paddingRight: 5,
-    borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   button2: {
     backgroundColor: "orange",
     borderRadius: 20,
     paddingLeft: 6,
     paddingRight: 6,
+    textAlign: "center",
+  },
+  editButtonContainer: {
+    flexDirection: "row",
+    paddingTop: 5,
   },
   priceGradeContainer: {
     flexDirection: "row",
