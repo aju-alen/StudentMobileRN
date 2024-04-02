e835dd80fb9f0d34b8010199320595bd9038ae18
Alen <alenaju1304@gmail.com>
2024-04-02 13:00:42 +0400

    Add BE url

--- client/app/utils/socket.js
+++ client/app/utils/socket.js
@@ -4,4 +4,4 @@ import {Platform} from 'react-native';
 
 export const BaseUrl  = () => Platform.OS === 'android' ? 'http://10.0.2.2:8081' : 'http://localhost:8081'; 
 
-export const socket = io.connect("https://studentmobilern.onrender.com");
+export const socket = io.connect("https://studentmobilern-31oo.onrender.com");
--- client/app/utils/utils.js
+++ client/app/utils/utils.js
@@ -1,3 +1,3 @@
-export const ipURL = "https://studentmobilern.onrender.com";
+export const ipURL = "https://studentmobilern-31oo.onrender.com";
 
 // http://10.65.1.122:3000
