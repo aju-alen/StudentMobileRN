4e7f7ae5c7cef880def633720b379c87797e3d5f
Alen <alenaju1304@gmail.com>
2024-05-21 12:21:12 +0400

    commented

---
+++ api/controllers/s3-controller.js
@@ -0,0 +1,19 @@
+// import User from "../models/user.js";
+// import crypto from "crypto";
+// import bcrypt from 'bcrypt';
+// import nodemailer from "nodemailer";
+// import jwt from "jsonwebtoken";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const postProfileImageS3 = async (req, res,next) => {
+//     console.log('req.body', req.body);
+//     try {
+
+//         res.status(200).json({ message:"looged reqbody" });
+//     }       
+//     catch (err) {
+//        next(err);
+//     }
+// }
+
---
+++ api/routes/s3route.js
@@ -0,0 +1,8 @@
+// import express from "express";
+// import { verifyToken } from "../middlewares/jwt.js";
+// import { postProfileImageS3 } from "../controllers/s3-controller.js";
+// const router = express.Router()
+
+// router.post('/post', postProfileImageS3);
+
+// export default router;
