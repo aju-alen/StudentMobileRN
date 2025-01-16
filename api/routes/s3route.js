e425f832da0da5fddc219461aff6af05171a7139
aju-alen <alenaju1304@gmail.com>
2025-01-16 22:14:47 +0400

    prisma skeleton

--- api/routes/s3route.js
+++ api/routes/s3route.js
@@ -1,10 +1,9 @@
 import express from "express";
 const router = express.Router()
 import { postProfileImageS3,subjectPDFVerifyeS3 } from '../controllers/s3-controller.js';
-import AWS from 'aws-sdk';
 import multer from 'multer';
-  // Configure multer for file upload
-  const upload = multer();
+// Configure multer for file upload
+const upload = multer();
 
 
 router.post('/upload-to-aws/:userId', upload.single('image'), postProfileImageS3);
