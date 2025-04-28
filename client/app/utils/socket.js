1401b88ce6900e7de03175125f36b9953d06bfb9
aju-alen <alenaju1304@gmail.com>
2025-04-28 13:58:34 +0400

    gitignore

--- client/app/utils/socket.js
+++ client/app/utils/socket.js
@@ -5,7 +5,8 @@ import {Platform} from 'react-native';
 export const BaseUrl  = () => Platform.OS === 'android' ? 'http://10.0.2.2:8081' : 'http://localhost:8081'; 
 
 // export const socket = io.connect("http://192.168.0.174:3000");
-export const socket = io.connect("https://studentmobilern-31oo.onrender.com");
+// export const socket = io.connect("https://studentmobilern-31oo.onrender.com");
+export const socket = io.connect(process.env.EXPO_PUBLIC_WS_URL);
 
 // https://studentmobilern-31oo.onrender.com 
 // backend url
--- client/app/utils/utils.js
+++ client/app/utils/utils.js
@@ -1,7 +1,9 @@
 // export const ipURL = "https://studentmobilern-31oo.onrender.com";
-export const ipURL = "http://10.65.1.237:3000";
+// export const ipURL = "http://10.65.1.237:3000";
 // export const ipURL = "http://10.65.1.75:3000";
 
 //https://studentmobilern-31oo.onrender.com
 
  //http://10.65.0.215:3000
+
+ export const ipURL = process.env.EXPO_PUBLIC_API_URL
